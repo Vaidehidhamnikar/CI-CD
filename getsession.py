d73477ed8d01abe6b8a79e0a3b2ca396e5edeef0
Vaidehidhamnikar <vaidehi.dhamnikar@kasmodigital.com>
2025-08-26 12:00:16 +0530

    Add CI/CD workflow for Snowflake Docker

---
+++ getsession.py
@@ -0,0 +1,36 @@
+import os
+import snowflake.connector
+from snowflake.snowpark import Session
+
+def connection() -> snowflake.connector.SnowflakeConnection:
+    """Returns a Snowflake connection object."""
+    token_path = "/snowflake/session/token"
+
+    if os.path.isfile(token_path):
+        creds = {
+            "host": os.getenv("SNOWFLAKE_HOST"),
+            "port": os.getenv("SNOWFLAKE_PORT"),
+            "protocol": "https",
+            "account": os.getenv("SNOWFLAKE_ACCOUNT"),
+            "authenticator": "oauth",
+            "token": open(token_path, "r").read(),
+            "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE"),
+            "database": os.getenv("SNOWFLAKE_DATABASE"),
+            "schema": os.getenv("SNOWFLAKE_SCHEMA"),
+        }
+    else:
+        creds = {
+            "account": os.getenv("SNOWFLAKE_ACCOUNT", "kcb69289"),
+            "user": os.getenv("SNOWFLAKE_USER", "VAIDEHI"),
+            "password": os.getenv("SNOWFLAKE_PASSWORD", "Kasmodigital@123"),
+            "role": os.getenv("SNOWFLAKE_ROLE", "ACCOUNTADMIN"),
+            "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE", "COMPUTE_WH"),
+            "database": os.getenv("SNOWFLAKE_DATABASE", "DASH_DB"),
+            "schema": os.getenv("SNOWFLAKE_SCHEMA", "DASH_SCHEMA"),
+        }
+
+    return snowflake.connector.connect(**creds)
+
+def create_session_object() -> Session:
+    """Returns a Snowpark session object."""
+    return Session.builder.configs({"connection": connection()}).create()
